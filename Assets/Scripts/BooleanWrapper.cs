ca54f4d82b19f7272190cca780f1be58a58d43ec
Maik <maki99999@web.de>
2020-12-31 13:16:45 +0100

    first imports

---
+++ Assets/Scripts/BooleanWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Default
+{
+    [System.Serializable]
+    public class BooleanWrapper : System.Object
+    {
+        public BooleanWrapper(bool val = false) { Value = val; }
+        public bool Value { get; set; }
+    }
+}
