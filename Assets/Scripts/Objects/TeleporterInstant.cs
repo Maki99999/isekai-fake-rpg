45e8f6ffc483a0d4f7350cef36b444112bc78230
Maik <maki99999@web.de>
2021-06-16 20:10:21 +0200

    improved transition between cave and non-cave incl. music change; added boss music

--- Assets/Scripts/Objects/TeleporterInstant.cs
+++ Assets/Scripts/Objects/TeleporterInstant.cs
@@ -9,16 +9,21 @@ namespace Default
         public Transform newPos;
         public AudioSource audioFx;
 
-        void OnTriggerEnter(Collider other)
+        protected virtual void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                GameController.Instance.eventManager.TeleportPlayer(true, newPos);
-
-                GameController.Instance.gameGuiFxAnimator.SetTrigger("Teleporter");
-                if (audioFx != null)
-                    audioFx.Play();
+                TeleportPlayer();
             }
+        }
+
+        protected virtual void TeleportPlayer()
+        {
+            GameController.Instance.eventManager.TeleportPlayer(true, newPos);
+
+            GameController.Instance.gameGuiFxAnimator.SetTrigger("Teleporter");
+            if (audioFx != null)
+                audioFx.Play();
         }
     }
 }
---
+++ Assets/Scripts/Objects/TeleporterSlow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Default
+{
+    public class TeleporterSlow : TeleporterInstant
+    {
+        protected override void OnTriggerEnter(Collider other)
+        {
+            base.OnTriggerEnter(other);
+        }
+
+        protected override void TeleportPlayer()
+        {
+            StartCoroutine(TeleportEvent());
+        }
+
+        private IEnumerator TeleportEvent() {
+            GameController.Instance.eventManager.FreezePlayer(true, true);
+            if (audioFx != null)
+                audioFx.Play();
+
+            GameController.Instance.gameGuiFxAnimator.SetTrigger("Cave");
+            yield return new WaitForSeconds(0.5f);
+            
+            GameController.Instance.eventManager.TeleportPlayer(true, newPos);
+            GameController.Instance.eventManager.FreezePlayer(true, false);
+        }
+    }
+}
