d8fc364a8d53e19c25a8563f0ef8576d410bc9a4
Maik <maki99999@web.de>
2022-05-27 18:09:26 +0200

    added second respawn position

--- Assets/Scripts/Player/GamePlayerDeath.cs
+++ Assets/Scripts/Player/GamePlayerDeath.cs
@@ -8,7 +8,7 @@ namespace Default
     {
         public PlayerController playerController;
         public Transform waitPosition;
-        public Transform respawnPosition;
+        public Transform[] respawnPositions;
 
         public PlayerStats playerStats;
 
@@ -23,17 +23,37 @@ namespace Default
             float hpRegenMultiplier = playerStats.statsRegen.hpRegenMultiplier;
             playerStats.statsRegen.hpRegenMultiplier = 0f;
             yield return new WaitForSeconds(1.5f);
+            Transform respawnPos = GetNearestRespawnPosition();
             playerController.TeleportPlayer(waitPosition);
 
             yield return new WaitForSeconds(5f);
             playerStats.ChangeHp(playerStats.maxHp);
-            playerController.TeleportPlayer(respawnPosition);
+            playerController.TeleportPlayer(respawnPos);
             playerStats.statsRegen.hpRegenMultiplier = hpRegenMultiplier;
             GameController.Instance.musicManager.ChangeMusic(MusicType.TRAVEL);
 
             yield return new WaitForSeconds(.5f);
             playerController.SetFrozen(false);
             gameObject.SetActive(false);
+        }
+
+        private Transform GetNearestRespawnPosition()
+        {
+            Vector3 playerPos = playerController.transform.position;
+            Transform nearestPos = playerController.transform;
+            float nearestDist = float.MaxValue;
+
+            foreach (Transform respawnPosition in respawnPositions)
+            {
+                float distanceSqr = (playerPos - respawnPosition.position).sqrMagnitude;
+                if (distanceSqr < nearestDist)
+                {
+                    nearestPos = respawnPosition;
+                    nearestDist = distanceSqr;
+                }
+            }
+
+            return nearestPos;
         }
     }
 }
